78e647099efa61ebf33560569a7f2f736a0412c2
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2024-10-22 20:17:13 +0200

    ADD added button innerText change on click

--- js/app.js
+++ js/app.js
@@ -3,10 +3,17 @@ const lampButton = document.querySelector('.toggle-lamp')
 lampButton.addEventListener('click', () => {
     const mainLamp = document.querySelector('.main-img')
     const altLamp = document.querySelector('.alt-img')
+    console.log(mainLamp.className)
 
-    mainLamp.classList.toggle('d-none')
-    mainLamp.classList.toggle('d-block')
-    
-    altLamp.classList.toggle('d-none')
-    altLamp.classList.toggle('d-block')
+    if (mainLamp.className.includes('d-none')) {
+        mainLamp.classList.toggle('d-none')
+        altLamp.classList.toggle('d-none')
+
+        lampButton.innerText = 'Accendi!'
+    } else {
+        mainLamp.classList.toggle('d-none')
+        altLamp.classList.toggle('d-none')
+
+        lampButton.innerText = 'Spegni!'
+    }
 })
